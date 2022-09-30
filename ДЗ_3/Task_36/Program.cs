e550a6615a92c64cbde4160e4c627048300ee974
skela <shaman1801@yandex.ru>
2022-09-30 14:09:18 +0300

    Task 36

---
+++ ДЗ_3/Task_36/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Task 36");
+
+ 
+
+Random rnd = new Random();
+
+int[] arr = new int[10];
+
+for(int i = 0; i < arr.Length; i++) arr[i] = rnd.Next();
+
+double sum = 0;
+
+for(int i = 1; i < arr.Length; i += 2) sum += arr[i];
+
+Console.WriteLine($"Сумма элементов: {sum}");
+
