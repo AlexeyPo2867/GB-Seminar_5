f1272ee1c7b733b60729c3dd7407279294e257b5
skela <shaman1801@yandex.ru>
2022-09-30 14:10:38 +0300

    Task 38

---
+++ ДЗ_3/Task_38/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Task 38");
+
+Random rnd = new Random();
+
+double[] arr = new double[10];
+
+for(int i = 0; i < arr.Length; i++) arr[i] = Math.Round(rnd.NextDouble() * 1000, 4);
+
+double dif = arr.Max() - arr.Min();
+
+Console.WriteLine($"Max {arr.Max()} Min {arr.Min()} -> разность {dif}");
